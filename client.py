50948512e55e5a6643917e44452aef885021e8d6
aditpatil10 <adit_patil@yahoo.com>
2020-06-30 19:55:52 +0530

    Fix [Bugs]

--- client.py
+++ client.py
@@ -4,31 +4,36 @@
 
 import socket  # for socket connection
 import os  # for renaming files
+import random  # python library for genrating random numbers
 import webbrowser  # for opening webpages
-import random  # to generate random numbers
 
-url = input()
+
+url = input()  # take the url from the user
+
 # creating client socket
 csocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-csocket.connect((socket.gethostname(), 8080))   # connecting to server
+csocket.connect((socket.gethostname(), 8080))   # connect to the server
+
 
 csocket.send(bytes(url, "UTF-8"))  # sending data to server
 
 
 msg = csocket.recv(1024)  # receiving message from client
+# decoding the message which was encoded by the server
 message = msg.decode("UTF-8")
 print("C: ", message)
+# Checking the message from server basically giving the green signal
 check = "HTTP/1.1 200 OK"
 if message == check:
     num = random.randint(1, 100)  # creating a random number for file
     msg = csocket.recv(1024)  # receiving message from server
-    filecontent = msg.decode("UTF-8")  # decoding message
+    filecontent = msg.decode("UTF-8")  # decoding the message
     f = open("index%s.txt" % num, 'w')
-    f.write(filecontent)    # writing contents into text file
+    f.write(filecontent)    # writing contents in the text file
     f.close()
     os.rename("index%s.txt" % num, "index%s.html" %
               num)  # renaming text file to html file
-    webbrowser.open("index%s.html" % num, new=2)  # open in new tab
+    webbrowser.open("index%s.html" % num, new=2)  # opening in new tab
     csocket.close()
 else:
     csocket.close()
--- server.py
+++ server.py
@@ -6,7 +6,7 @@ import threading
 
 
 url1 = "http://localhost:8080/index.html"
-url2 = "http://localhost:8080/"         # storing the correct urls for comparison
+url2 = "http://localhost:8080/"         # URLS for request and comparison
 # Create a TCP server socket
 
 
@@ -18,7 +18,7 @@ serverPort = 8080
 
 # Bind the socket to server address and server port
 
-serversocket.bind((socket.gethostname(), serverPort))
+serverSocket.bind((socket.gethostname(), serverPort))
 
 # Listen to at most 5 connection at a time
 
@@ -29,30 +29,30 @@ serverSocket.listen(5)
 
 def multi_thread(connectionSocket):
     if clienturl == url1 or clienturl == url2 or clienturl == url3:
-        # Extract the path of the requested object from the message
+        # Extracting the path of the requested object from the message
         clientsocket.send(bytes("HTTP/1.1 200 OK", "UTF-8"))
-        filename = 'index.html'  # sending html page to client
+        filename = 'index.html'  # sending web page to client
         f = open(filename, 'rb')
         l = f.read(1024)
         while (l):
             clientsocket.send(l)
             print('S: Sent data to client sucessfully ')
             l = f.read(1024)
-        f.close()  # closing the socket once file is sent
-        clientsocket.close()  # closing the socket once data is sent
+        f.close()  # closing socket
+        clientsocket.close()  # closing socket
     else:
         print(f'S: Failed to connect with client with address {address} ')
         # sending failure header to client
         clientsocket.send(bytes("HTTP/1.1 404 Not Found", "UTF-8"))
-        clientsocket.close()  # closing the socket once  data is sent
+        clientsocket.close()  # closing socket
 
 
 while True:
-    clientsocket, address = serversocket.accept()
+    clientsocket, address = serverSocket.accept()
     print(
         f"S: Connection Successful! Connected to client with address {address}")
-    msg = clientsocket.recv(1024)  # receiving message from client
+    msg = clientsocket.recv(1024)       # receiving message from client
     clienturl = msg.decode("UTF-8")
-    # creating thread for every client coming in
-    t = threading.Thread(target=task1, name='t', args=(clienturl,))
+    # creating the thread for every client
+    t = threading.Thread(target=multi_thread, name='t', args=(clienturl,))
     t.start()
